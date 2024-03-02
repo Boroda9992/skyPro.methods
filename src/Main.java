295cf8784f08f7bd84295a8d8d9f77e6b680735f
Boroda9992 <fedko.ak@gmail.com>
2024-03-02 14:54:51 +0300

    ДЗ по методам. Работа над ошибками. - Главной ошибкой, естественно, было неправильное понимание методов. По какой-то причине мне казалось, что если метод принимает на вход данные, то я должен указать их тип. Отсюда и бессмысленный вывод методов, который я сам не понимал как работает. Отправил на проверку, по большей части для того, чтобы мне подсказали где искать ошибку. - 12 - 21 - Убрал вложенность, как смог. Проверил на каждый вывод - работает корректно. - Переработал третью задачу. Метод получает int расстояние; в зависимости от условий, записывает в переменную значение дней доставки; возвращает эту переменную. Дальше просто вывод строки + переменная. - Изменил имена методов, согласно общей логике. Познакомился с сочетанием клавиш ctrl+F6 на практике - удобно и дополнительно страхует от ошибок. Ознакомился с понятием сигнатуры метода.

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,37 @@
 public class Main {
 
-    public static int calculatingLeapYear(int year) {
+    public static void calculateLeapYear(int year) {
         boolean leapYear = (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)) && (year > 1584);
         if (leapYear == true) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
         }
-        return year;
     }
 
-    public static int versionChecker(int clientOS, int clientDeviceYear) {
-        if (clientOS == 0) {
-            if (clientDeviceYear < 2024) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
+    public static void suggestVersion(int clientOS, int clientDeviceYear) {
+        if (clientOS == 0 && clientDeviceYear < 2024) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear < 2024) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1) {
-            if (clientDeviceYear < 2024) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
-        return clientDeviceYear;
     }
 
-    public static int distanceCalculator(int deliveryDistance) {
+    public static int calculateDeliveryDays(int deliveryDistance) {
         int deliveryDays = 0;
-        if (deliveryDistance<20) {
+        if (deliveryDistance < 20) {
             deliveryDays = deliveryDays + 1;
-            System.out.println(deliveryDistance + "км - это первая зона доставки. Потребуется дней: " + deliveryDays);
-        } else if (deliveryDistance<60) {
+        } else if (deliveryDistance < 60) {
             deliveryDays = deliveryDays + 2;
-            System.out.println(deliveryDistance + "км - это вторая зона доставки. Потребуется дней: " + deliveryDays);
-        } else if (deliveryDistance<100) {
+        } else if (deliveryDistance < 100) {
             deliveryDays = deliveryDays + 3;
-            System.out.println(deliveryDistance + "км - это третья зона доставки. Потребуется дней: " + deliveryDays);
-        } else {
-            System.out.println("До вас: " + deliveryDistance + "км." + " К сожалению, свыше 100 км доставки нет.");
         }
-        return deliveryDistance;
+        return deliveryDays;
     }
 
     public static void main(String[] args) {
@@ -50,23 +40,24 @@ public class Main {
         System.out.println("Задача 1");
         System.out.println();
 
-        int year = 2024;
-        calculatingLeapYear(year);
+        int year = 2023;
+        calculateLeapYear(year);
 
         System.out.println();
         System.out.println("Задача 2");
         System.out.println();
 
-        int clientDeviceYear = 2022;
+        int clientDeviceYear = 2024;
         int clientOS = 0;
-        versionChecker(clientOS, clientDeviceYear);
+        suggestVersion(clientOS, clientDeviceYear);
 
         System.out.println();
         System.out.println("Задача 3");
         System.out.println();
 
         int deliveryDistance = 95;
-        distanceCalculator(deliveryDistance);
+        int deliveryDays = calculateDeliveryDays(deliveryDistance);
+        System.out.println("Потребуется дней: " + deliveryDays);
 
         System.out.println();
     }
